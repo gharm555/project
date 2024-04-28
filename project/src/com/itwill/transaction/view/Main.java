691b82bf0866be10ec5039e0efcf3c39871320e1
김대현 <phm2530@naver.com>
2024-04-28 15:00:28 +0900

    1

--- project/src/com/itwill/transaction/view/Main.java
+++ project/src/com/itwill/transaction/view/Main.java
@@ -2,6 +2,9 @@ package com.itwill.transaction.view;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Main {
     private JFrame frame;
@@ -26,10 +29,36 @@ public class Main {
     private void initialize() {
         frame = new JFrame();
         frame.setTitle("Transaction Management");
-        frame.setBounds(100, 100, 450, 300);
+        frame.setBounds(100, 100, 393, 852);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(null);
         frame.setResizable(false);
+        frame.getContentPane().setLayout(null);
+        
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 377, 813);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
+        
+        JLabel lbl자산제목 = new JLabel("자산");
+        lbl자산제목.setHorizontalAlignment(SwingConstants.CENTER);
+        lbl자산제목.setBounds(12, 10, 353, 49);
+        panel.add(lbl자산제목);
+        
+        JLabel lblNewLabel = new JLabel("New label");
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setBounds(11, 69, 110, 49);
+        panel.add(lblNewLabel);
+        
+        JLabel lblNewLabel_1 = new JLabel("New label");
+        lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel_1.setBounds(132, 69, 110, 49);
+        panel.add(lblNewLabel_1);
+        
+        JLabel lblNewLabel_2 = new JLabel("New label");
+        lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel_2.setBounds(253, 69, 110, 49);
+        panel.add(lblNewLabel_2);
 
         
     }
