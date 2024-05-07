9d2bffcd825d18c343a4d90275d1688dccae99bb
gharm555 <phm2530@naver.com>
2024-05-07 09:06:05 +0900

    1

--- project/src/com/itwill/transaction/Jdbc.java
+++ project/src/com/itwill/transaction/Jdbc.java
@@ -1,7 +1,7 @@
 package com.itwill.transaction;
 
 public interface Jdbc {
-	String URL = "jdbc:oracle:thin:@192.168.1.4:1521/xe";
-	String USER = "system";
+	String URL = "jdbc:oracle:thin:@192.168.20.28:1521/xe";
+	String USER = "scott";
 	String PASSWORD = "tiger";
 }
